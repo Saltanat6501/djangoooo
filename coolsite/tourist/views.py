2cdc9b8a909cc3f55b57362f15e6a45994dfc88b
Salta <saltanatrakhmetova2004@gmail.com>
2023-04-23 18:06:56 +0600

    first commit

--- coolsite/tourist/views.py
+++ coolsite/tourist/views.py
@@ -1,38 +1,67 @@
-from django.http import HttpResponse,HttpResponseNotFound,Http404
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
-menu = [{'title': "Сайт туралы",'url_name': 'about'},
-        {'title': "Қосу",'url_name': 'add_page'},
-        {'title': "Кері байланыс", 'url_name': 'contact'},
-        {'title': "Кіру", 'url_name': 'login'}
-       ]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+
 
 def index(request):
-    posts=Tourist.objects.all()
-    context={
+    posts = Tourist.objects.all()
+
+    context = {
         'posts': posts,
         'menu': menu,
-        'title': 'Басты бет'
+        'title': 'Главная страница',
+        'cat_selected': 0,
     }
 
-    return render(request, 'tourist/index.html', context)
+    return render(request, 'tourist/index.html', context=context)
 
 def about(request):
-    return render(request, 'tourist/about.html', {'menu': menu, 'title': 'Сайт туралы'})
+    return render(request, 'tourist/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def addpage(request):
-    return HttpResponse("қосу")
+    return HttpResponse("Добавление статьи")
 
 def contact(request):
-    return HttpResponse("Кері байланыс")
+    return HttpResponse("Обратная связь")
 
 def login(request):
-    return HttpResponse("Кіру")
+    return HttpResponse("Авторизация")
 
-def pageNotFound(request,exception):
+
+def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request,post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Tourist, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'tourist/post.html', context=context)
+
+def show_category(request, cat_id):
+    posts = Tourist.objects.filter(cat_id=cat_id)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'tourist/index.html', context=context)
